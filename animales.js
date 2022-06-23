5e81a8cef66d9053aa1c8d6cdc0ce172543a0c45
JulSarmiento <jasa1999@hotmail.com>
2022-06-23 12:26:52 -0500

    agregado documentacion basica

--- animales.js
+++ animales.js
@@ -2,6 +2,15 @@ let perros = [];
 let gatos = []; 
 
 class Mamifero {
+    /**
+     * 
+     * @param {boolean} habitat 
+     * @param {string} homeotermo 
+     * @param {string} extremidades 
+     * @param {number} parentalidad 
+     * @param {string} especie 
+     * @param {boolean} domestico 
+     */
     constructor(habitat, homeotermo, extremidades, parentalidad, especie, domestico){
         this.habitat = habitat; 
         this.homeotermo = homeotermo;
@@ -13,30 +22,60 @@ class Mamifero {
 };
 
 class Perro extends Mamifero {
+    /**
+     * 
+     * @param {boolean} habitat 
+     * @param {string} homeotermo 
+     * @param {string} extremidades 
+     * @param {number} parentalidad 
+     * @param {string} especie 
+     * @param {boolean} domestico 
+     * @param {string} nombre 
+     * @param {number} edad 
+     */
     constructor(habitat, homeotermo, extremidades, parentalidad, especie, domestico, nombre, edad) {
         super(habitat, homeotermo, extremidades, parentalidad, especie, domestico);
         this.nombre = nombre;
         this.edad = edad;
     }
     
+    /**
+     * 
+     * @returns array
+     */
     static guardarPerrito() {
 
-        const perro = new Perro('No tiene', 'Sangre caliente', 'Patas', '8 semanas', 'canino', 'si', 'Odashi', '4 años');
+        const perro = new Perro(false, 'Sangre caliente', 'Patas', 8, 'canino', true, 'Odashi', 4);
         perros.push(perro);
         return perros
     }
 }
 
 class Gato extends Mamifero {
+    /**
+     * 
+     * @param {boolean} habitat 
+     * @param {string} homeotermo 
+     * @param {string} extremidades 
+     * @param {number} parentalidad 
+     * @param {string} especie 
+     * @param {boolean} domestico 
+     * @param {string} nombre 
+     * @param {number} edad 
+     */
     constructor(habitat, homeotermo, extremidades, parentalidad, especie, domestico, nombre, edad){
         super(habitat, homeotermo, extremidades, parentalidad, especie, domestico);
         this.nombre = nombre;
         this.edad = edad;
     }
 
+    /**
+     * 
+     * @returns array
+     */
     static guardarGatito() {
 
-        const gato = new Gato('No tiene', 'Sangre caliente', 'Patas', '12 semanas', 'felino', 'si', 'Vader', '2 años');
+        const gato = new Gato(false, 'Sangre caliente', 'Patas', 8, 'felino', true, 'Vader', 4);
         gatos.push(gato);
         return gatos
     }
